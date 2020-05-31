6ee3ef2f39f94abc545cce9f6338add37dd6f05e
bekalu12 <bekaluyirgalem85@gmail.com>
2020-05-30 22:17:27 -0500

    2

--- lab7/bank.js
+++ lab7/bank.js
@@ -1,23 +1,24 @@
-
-//function newaccounttype(){
-document.getElementById("new").onclick = function(evt){
-    evt.preventDefault();
-    accountInfoList.push(createAccount());
-    document.getElementById("output").innerHTML = "";
-    for (let i = 0; i < accountInfoList.length; i++) {
-        document.getElementById("output").innerHTML += 
-        `Account name: ${accountInfoList[i].name} and balance: ${accountInfoList[i].balance} \n`; 
+(function() {
+    "use strict";
+    document.getElementById("new").onclick = function(evt){
+        evt.preventDefault();
+        accountInfoList.push(createAccount());
+        document.getElementById("output").innerHTML = "";
+        for (let i = 0; i < accountInfoList.length; i++) {
+            document.getElementById("output").innerHTML += 
+            `Account name: ${accountInfoList[i].name} and balance: ${accountInfoList[i].balance} \n`; 
+        }
     }
-}
-
-const accountInfoList = [];
-
-const createAccount = function() {
-    let accountName = document.getElementById("acctype").value;
-    let depositAmount = document.getElementById("depo").value;
-
-    return {
-        name : accountName,
-        balance : depositAmount
+    
+    const accountInfoList = [];
+    
+    const createAccount = function() {
+        let accountName = document.getElementById("acctype").value;
+        let depositAmount = document.getElementById("depo").value;
+    
+        return {
+            name : accountName,
+            balance : depositAmount
+        }
     }
-}
+})();
