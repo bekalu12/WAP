b643140d42188bf83266902bf08e12ea91c58a8d
bekalu12 <bekaluyirgalem85@gmail.com>
2020-06-04 16:09:41 -0500

    first

---
+++ lab10/lab10.js
@@ -0,0 +1,24 @@
+"use strict";
+
+$(function() {
+	$('#hw').change(function() {
+		console.log("kghkk");
+		var file = 'homeworks/'+$('#hw').val();
+		$.ajax({
+			'url': file,
+			'type': 'GET',
+			'success': ajaxSuccess,
+			'error': ajaxFailure
+		});
+	});
+});
+
+function ajaxSuccess(data) {
+	$('#output').val(data);
+}
+
+function ajaxFailure(xhr, status, exception) {
+  console.log(xhr, status, exception);
+}
+
+$.get('https://www.google.com/images/branding/product/ico/googleg_lodp.ico');
--- lab9/fifteen.js
+++ lab9/fifteen.js
@@ -1,33 +1,86 @@
-    // $("body, button").css({
-	// "font-family": "cursive",
-	// "font-size": "14pt",
-	// "text-align": "center"
-    // });
+    $("body, button").css({
+	"font-family": "cursive",
+	"font-size": "14pt",
+	"text-align": "center"
+    });
    
-    // $("#puzzlearea").children().css({
-    //     "float":"left",
-    //     "border":"5px solid black",
-    //     "height":"90px",
-    //     "width":"90px",
+    $("#puzzlearea").children().css({
+        "float":"left",
+        "border":"5px solid black",
+        "height":"90px",
+        "width":"90px",
        
-    //     //,"position": "absolute"
-    // });
-    // $("#puzzlearea").css({
-    //     "font-size": "40pt",
-	// "width": "400px",
-	// "height":"400px",
-	// "margin-left": "auto",
-	// "margin-right": "auto",
-    // "position": "relative"
-    // ,"background-image": "url(background.jpg)"
+        //,"position": "absolute"
+    });
+    $("#puzzlearea").css({
+        "font-size": "40pt",
+	"width": "400px",
+	"height":"400px",
+	"margin-left": "auto",
+	"margin-right": "auto",
+    "position": "relative"
+    ,"background-image": "url(background.jpg)"
 
 
-    //  });
+     });
     // .movablepiece:hover {
     //     border-color: red;
     //     color: red;
     // }
 
-$("#puzzlearea").children().each(function() {
-    $(this).addClass("puzzlepiece");
+// $("#puzzlearea").children().each(function() {
+//     $(this).addClass("puzzlepiece");
+    
+// })
+// /* 
+// init = function() {
+//     var puzzleArea = document.getElementById('puzzlearea');
+//     var divs = puzzleArea.getElementsByTagName("div");
+      
+//     // initialize each piece
+//     for (var i=0; i< divs.length; i++) {
+//         var div = divs[i];
+        
+//         // calculate x and y for this piece
+//         var x = ((i % 4) * 100) ;
+//         var y = (Math.floor(i / 4) * 100) ;
+
+//         // set basic style and background
+//         div.className = "puzzlepiece";
+//         div.style.left = x + 'px';
+//         div.style.top = y + 'px';
+//         div.style.backgroundImage = 'url("background.jpg")';
+//         div.style.backgroundPosition = -x + 'px ' + (-y) + 'px';
+        
+//         // store x and y for later
+//         div.x = x;
+//         div.y = y; 
+//     }        
+// };
+window.onload = function() {
+	$("shufflebutton").observe('click', shufflePuzzleEvent);
+	populateTiles();
+}
+init = $();
+var puzzleArea = $("#puzzlearea");
+var divs = puzzleArea. $("div");
+// initialize each piece
+$("divs").each(function(index,value){
+    var div = divs[value];
+    // calculate x and y for this piece
+         var x = ((index % 4) * 100) ;
+         var y = (Math.floor(index / 4) * 100) ;
+
+         // set basic style and background
+         $("div").addClass("className") = "puzzlepiece";
+         $("div").css({
+            "left": "x + 'px'",
+            "top": "y + 'px'",
+            "backgroundImage" :'url("background.jpg")',
+            "backgroundPosition" : "-x + 'px ' + (-y) + 'px'" 
+         });
+    // store x and y for later
+         div.x = x;
+         div.y = y; 
+
 })
